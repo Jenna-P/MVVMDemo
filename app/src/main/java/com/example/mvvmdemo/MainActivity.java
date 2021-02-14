bd276ccd2901af56a570b0b7e1635b787db1d209
jenna-P <jenn2464@edu.easj.dk>
2021-02-14 18:12:39 +0100

    Added EditText, Button and TextView

--- app/src/main/java/com/example/mvvmdemo/MainActivity.java
+++ app/src/main/java/com/example/mvvmdemo/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.mvvmdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    Button btnOK;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText = (EditText)findViewById(R.id.editText);
+        btnOK = (Button)findViewById(R.id.btnOK);
+        textView = (TextView)findViewById(R.id.textView);
+
+        btnOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String inputText = editText.getText().toString();
+                textView.setText(inputText);
+            }
+        });
     }
 }
