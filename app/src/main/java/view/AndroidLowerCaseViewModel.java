f0295aae264f68a6da058aff4a7c20e67d4b3621
jenna-P <jenn2464@edu.easj.dk>
2021-02-26 14:48:23 +0100

    Created AndroidLowerCaseViewModel class

--- app/src/main/java/view/AndroidLowerCaseViewModel.java
+++ app/src/main/java/view/AndroidLowerCaseViewModel.java
@@ -1,5 +1,44 @@
 package view;
 
-public class AndroidLowerCaseViewModel {
+import androidx.lifecycle.MutableLiveData;
 
+import java.util.Observable;
+import java.util.Observer;
+
+import model.Model;
+
+public class AndroidLowerCaseViewModel {
+    private Model model = new Model();
+    private MutableLiveData<String> pd = new MutableLiveData<>();
+
+    public AndroidLowerCaseViewModel() {
+        observeModel(model);
+        pd.setValue(getTransformedData(model.getData()));
+
+
+    }
+
+    private void observeModel(Model model) {
+        model.addObserver(new Observer() {
+            @Override
+            public void update(Observable o, Object arg) {
+                if (o instanceof Model) {
+                    String data = ((Model) o).getData();
+                    pd.setValue(getTransformedData(data));
+                }
+            }
+        });
+    }
+
+    private String getTransformedData(String data) {
+        return data.toLowerCase();
+    }
+
+    public MutableLiveData<String> getPd() {
+        return pd;
+    }
+
+    public void setPd(MutableLiveData<String> pd) {
+        this.pd = pd;
+    }
 }
